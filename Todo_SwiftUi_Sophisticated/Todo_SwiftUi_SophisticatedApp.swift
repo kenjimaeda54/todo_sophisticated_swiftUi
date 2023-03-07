a978e7bb8c43e7af416c2e3fcaef76b8d497574b
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-03-06 21:34:51 -0300

    Initial Commit

---
+++ Todo_SwiftUi_Sophisticated/Todo_SwiftUi_SophisticatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftUi_SophisticatedApp.swift
+//  Todo_SwiftUi_Sophisticated
+//
+//  Created by kenjimaeda on 06/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftUi_SophisticatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
