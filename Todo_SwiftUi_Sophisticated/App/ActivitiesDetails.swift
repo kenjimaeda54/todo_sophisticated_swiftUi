969e0b47c7c8e3c44dd0f20f0995542e61220dce
Kenji <31631601+kenjimaeda54@users.noreply.github.com>
2023-03-28 19:18:06 -0300

    feat: started pod file

--- Todo_SwiftUi_Sophisticated/App/ActivitiesDetails.swift
+++ Todo_SwiftUi_Sophisticated/App/ActivitiesDetails.swift
@@ -12,10 +12,31 @@ struct ActivitiesDetails: View {
 	@Binding  var categoryActivies: CategoryActivities?
 	@State private var  activies: CategoryActivities? = nil
 	@State private var textField = ""
+	@State private var openDatePicker = false
+	@State private var date = Date()
+	@State private var showingDatePicker = false
+	let  startDate = Calendar.current.startOfDay(for: Date())
+	//se não fizer abaixo como propriedade computada ira reclamar , pois preciso que start date esteja pronto
+	//	When should I use a Computed Property?
+	//	It’s important to understand when you should use a Computed Property over a stored property. Use it when:
+	//
+	//	It depends on other properties
+	//	You’re defining the property inside an extension
+	//	The property is an access point to another object without disclosing it fully
+	//https://www.avanderlee.com/swift/computed-property/#:~:text=Computed%20properties%20are%20part%20of,computes%20its%20property%20upon%20request.
+	var  endDate: Date  {
+		Calendar.current.date(byAdding: DateComponents(year: 1), to: startDate)?.addingTimeInterval(-1) ?? Date()
+	}
 	
 	func handleAddActivy() {
 		
 	}
+	
+	func handleOpenDatePicker() {
+		showingDatePicker.toggle()
+		print(startDate,endDate)
+	}
+
 	
 	
 	var body: some View {
@@ -28,11 +49,16 @@ struct ActivitiesDetails: View {
 							.foregroundColor(ColorsApp.secondaryColor)
 						Spacer()
 						HStack{
-							Text("25 Feb")
-								.font(.custom("Inter-Medium", size: 17))
-								.foregroundColor(ColorsApp.secondaryColor)
-							Image(systemName: "calendar")
-								.frame(width: 20,height: 20)
+							Button(action: handleOpenDatePicker){
+								Text("25 Feb")
+									.font(.custom("Inter-Medium", size: 17))
+									.foregroundColor(ColorsApp.secondaryColor)
+								Image(systemName: "calendar")
+									.frame(width: 20,height: 20)
+									.foregroundColor(ColorsApp.secondaryColor)
+							}
+							
+							
 							
 						}//HStack
 						.padding(.vertical,10)
@@ -43,7 +69,7 @@ struct ActivitiesDetails: View {
 					}// HStack
 					Spacer()
 					TextField(text: $textField) {
-						 Text("Your description,it's optional")
+						Text("Your description,it's optional")
 					}
 					.font(.custom("Inter-Regular", size: 18))
 					.foregroundColor(ColorsApp.black)
@@ -54,20 +80,29 @@ struct ActivitiesDetails: View {
 					Spacer()
 					
 					ButtonDefault(titleButton: "Add Activity", actionButton: handleAddActivy)
-						.offset(y: -3w0)
-				
+						.offset(y: -30)
+					if(showingDatePicker) {
+						DatePicker("", selection: $date, in:  Date()...,displayedComponents: [.date])
+							.datePickerStyle(.wheel)
+						 
+					}
+					
 				}// VStack
 				.padding(.top,10)
 				.padding(.horizontal,20)
 				.background(ColorsApp.primaryColor)
+				
+				
+				
+				
 			}// NavigationStck
-	
+			
 		} // Geometry
 		.toolbar(.hidden,for: .navigationBar)
 		.onAppear(){
 			activies = categoryActivies
 		}
-	
+		
 	}
 	
 	
@@ -78,6 +113,6 @@ struct ActivitiesDetails_Previews: PreviewProvider {
 	static var previews: some View {
 		let categoriesActives = CategoryActivities(id: 11, title: "Loving", image: "read")
 		ActivitiesDetails(categoryActivies: .constant(categoriesActives))
-	
+		
 	}
 }
